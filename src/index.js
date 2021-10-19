6315bec93898dd5660b0ef3a03d73e092ba2ac6a
YigitaliyevIslomjon <myresult1998@gmail.com>
2021-10-19 19:28:24 +0500

    nav change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import HomePage from './components/HomePage/HomePage';
+// import HomePage from './components/HomePage/HomePage';
+import ProductsHover_inner from './components/ProductsHover_inner/ProductsHover_inner';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css'
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <HomePage/>
+      {/* <HomePage/> */}
+      <ProductsHover_inner/>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
