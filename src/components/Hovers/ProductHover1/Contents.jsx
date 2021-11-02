19958646f7b33f4bd3c5195056908af8cfb6b8e9
YigitaliyevIslomjon <myresult1998@gmail.com>
2021-11-02 17:16:26 +0500

    changes

--- src/components/Hovers/ProductHover1/Contents.jsx
+++ src/components/Hovers/ProductHover1/Contents.jsx
@@ -12,8 +12,8 @@ function Contents(props) {
 
     const stil={
       transform:"rotate(180deg)",
-    //   background:'blue',
-      fill:'white'
+      backgroundColor:'rgba(30, 89, 164, 1)',
+      
     }
     const stil1={
       transform:"rotate(0deg)"
@@ -23,6 +23,7 @@ function Contents(props) {
         setShow1(prev=>!prev)
         setShow2(prev=>!prev)
         setTransform(prev=>!prev)
+        
       }
     return (
         <>
@@ -31,11 +32,11 @@ function Contents(props) {
                <h1>{props.data.title}</h1>
                <hr className={"hr1"} />
                </div>
-                <div  style={transform===true ?stil:stil1 }  className="arrow1">
-                <svg  width="11" height="13" viewBox="0 0 11 13"  xmlns="http://www.w3.org/2000/svg">
+                <div  style={transform===true ? stil:stil1 }  className="arrow1">
+                <svg  id='svg' width="11" height="13" viewBox="0 0 11 13"  xmlns="http://www.w3.org/2000/svg">
                     <g>
-<path id="bgcolor"  d="M5.55273 1.3353L5.55273 11.6216" stroke="#1E59A4" stroke-width="1.5677" stroke-linecap="round" stroke-linejoin="round"/>
-<path id="bgcolor" d="M9.76074 7.41356L5.55272 11.6216L1.34469 7.41356" stroke="#1E59A4" stroke-width="1.5677" stroke-linecap="round" stroke-linejoin="round"/>
+<path   d="M5.55273 1.3353L5.55273 11.6216" stroke="#1E59A4" stroke-width="1.5677" stroke-linecap="round" stroke-linejoin="round"/>
+<path   d="M9.76074 7.41356L5.55272 11.6216L1.34469 7.41356" stroke="#1E59A4" stroke-width="1.5677" stroke-linecap="round" stroke-linejoin="round"/>
 </g>
 </svg>
                 </div> 
