70739dc0b85be5df7d6b1b5285a1c6784e610ca8
YigitaliyevIslomjon <myresult1998@gmail.com>
2021-11-03 02:10:29 +0500

    changes

--- src/components/ProductsHover_inner/Squre.js
+++ src/components/ProductsHover_inner/Squre.js
@@ -3,7 +3,7 @@ import './Squre.css'
 function Squre() {
     return (
        <>
-            <button><font className='rang1'>1P</font><font className='rang2'>(195)</font></button>
+            <button className='squreRang'><font className='rang1'>1P</font><font className='rang2'>(195)</font></button>
        </>
     )
 }
--- src/components/ProductsHover_inner2/Product_container.js
+++ src/components/ProductsHover_inner2/Product_container.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import './product_container.css'
 import rasm2 from '../assets/images/rasm2.png'
-import strelka from '../assets/images/strelka.png'
+import strelka1 from '../assets/icons/strelka1.svg'
+import strelka1_2 from '../assets/icons/strelka1_2.svg'
 import { Link } from 'react-router-dom'
 function Product_container() {
     return (
-        <Link to='/avtomaticheskiy'>
-        <div className={'product_container'}>
+        <>
+        <Link to='/avtomaticheskiy' className={'pro_hover_inner2_box'}>
                 <img src={rasm2} alt='rasm'/>
-                <div className='product_title'>
+                <div className='pro_hover_inner2_box_title'>
                     <p>284003</p>
                     <p>Автоматический выключатель NB1-63 1Р 6А 6кА х-ка C (CHINT)</p>
                 </div>
-                <img src={strelka} alt='rasm'/>
-        </div>
+                <object
+                className='pro_hover_inner2_box_svg1'
+                data={strelka1}
+                type="image/svg+xml"
+                >
+                </object>
+                <object
+                className='pro_hover_inner2_box_svg1_2'
+                data={strelka1_2}
+                type="image/svg+xml"
+                >
+                </object>
         </Link>
+        </>
     )
 }
 
