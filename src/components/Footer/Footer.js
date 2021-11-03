02a1ed9b3002e59356678ba47e03b4ba7c520a5e
publicStart <bahrom.tuit1996@gmail.com>
2021-11-03 17:26:08 +0500

    problem solved2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,44 +4,43 @@ function Footer() {
   return (
     <>
       <footer id="footer">
-      <div class="footer_container">
-        <div class="footer_columns">
-          <div class="footer_column">
-            <h2 class="footer_col_title">Низковольтное оборудование</h2>
-            <div class="low_volt_links">
-              <a class="low_volt_link" href="#">Низковольтное оборудование</a>
+      <div className="footer_container">
+        <div className="footer_columns">
+          <div className="footer_column">
+            <h2 className="footer_col_title">Низковольтное оборудование</h2>
+            <div className="low_volt_links">
+              <a className="low_volt_link" href>Низковольтное оборудование</a>
               <br />
-              <a class="low_volt_link" href="#">Модульное оборудование</a>
-              <br />
-              <a class="low_volt_link" href="#"
-                >Силовое оборудование защиты и коммутации</a
+              <a className="low_volt_link" href>Модульное оборудование</a>
+              <br /> 
+              <a className="low_volt_link" href>Силовое оборудование защиты и коммутации</a
               >
               <br />
-              <a class="low_volt_link" href="#">Контакторы, реле, пускатели</a>
+              <a className="low_volt_link" href>Контакторы, реле, пускатели</a>
               <br />
-              <a class="low_volt_link" href="#"
+              <a className="low_volt_link" href
                 >Компенсации реактивной мощности</a
               >
               <br />
-              <a class="low_volt_link" href="#"
+              <a className="low_volt_link" href
                 >Кнопки управления, индикаторы</a
               >
               <br />
-              <a class="low_volt_link" href="#">Оборудование электропитания</a>
+              <a className="low_volt_link" href>Оборудование электропитания</a>
               <br />
-              <a class="low_volt_link" href="#"
+              <a className="low_volt_link" href
                 >Выключатель-разъединители/рубильники</a
               >
               <br />
-              <a class="low_volt_link" href="#">Приводная техника</a>
+              <a className="low_volt_link" href>Приводная техника</a>
               <br />
-              <a class="low_volt_link" href="#">Щиты и шкафы</a>
+              <a className="low_volt_link" href>Щиты и шкафы</a>
               <br />
-              <a class="low_volt_link" href="#"
+              <a className="low_volt_link" href
                 >Аксессуары для шкафов и щитов</a
               >
               <br />
-              <a class="low_volt_link" href="#"
+              <a className="low_volt_link" href
                 >Цифровые измерительные приборы
               </a>
             </div>
@@ -62,23 +61,23 @@ function Footer() {
               <br />
               <button class="footer_oultined_btn">Скачать каталог</button>
             </div>
-            <div class="low_volt_links">
-              <a class="low_volt_link" href="#">О компании </a>
+            <div className="low_volt_links">
+              <a className="low_volt_link" href>О компании </a>
               <br />
-              <a class="low_volt_link" href="#">Скачать</a>
+              <a className="low_volt_link" href>Скачать</a>
               <br />
-              <a class="low_volt_link" href="#">Где купить </a>
+              <a className="low_volt_link" href>Где купить </a>
               <br />
-              <a class="low_volt_link" href="#">Мероприятия</a>
+              <a className="low_volt_link" href>Мероприятия</a>
               <br />
-              <a class="low_volt_link" href="#">Поддержка</a>
+              <a className="low_volt_link" href>Поддержка</a>
               <br />
-              <a class="low_volt_link" href="#">Контакты</a>
+              <a className="low_volt_link" href>Контакты</a>
             </div>
             <div class="footer_socials">
               <div class="footer_social_circle">
                 <object
-                  class="map_svg"
+                  className="map_svg"
                   class="advantage_cards__icon2"
                   data="./assets/icons/facebook.svg"
                   type="image/svg+xml"
@@ -87,7 +86,7 @@ function Footer() {
               <div class="footer_social_circle">
                 <object
                   class="map_svg"
-                  class="advantage_cards__icon2"
+                  className="advantage_cards__icon2"
                   data="./assets/icons/twitter.svg"
                   type="image/svg+xml"
                 > </object>
@@ -95,7 +94,7 @@ function Footer() {
               <div class="footer_social_circle">
                 <object
                   class="map_svg"
-                  class="advantage_cards__icon2"
+                  className="advantage_cards__icon2"
                   data="./assets/icons/telegram.svg"
                   type="image/svg+xml"
                 > </object>
@@ -103,7 +102,7 @@ function Footer() {
               <div class="footer_social_circle">
                 <object
                   class="map_svg"
-                  class="advantage_cards__icon2"
+                  className="advantage_cards__icon2"
                   data="./assets/icons/insta.svg"
                   type="image/svg+xml"
                 > </object>
